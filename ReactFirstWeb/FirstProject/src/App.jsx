b9dd02976d97fef29f3cce280dc39d609b5a23b9
Sajid416 <sajidulislam6183@gmail.com>
2025-04-28 11:34:00 +0600

    Your meaningful commit message

--- ReactFirstWeb/FirstProject/src/App.jsx
+++ ReactFirstWeb/FirstProject/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-     <h1 className='text-red-500'>This is our header tag</h1>
+     <h1 className='text-red-200 flex px-2.5 text-5xl mx py-1.5'>This is our header tag</h1>
         
     </>
   )
